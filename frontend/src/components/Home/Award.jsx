cb2886a9c0c5bcf95e41d86e51ee0f83155f7fa7
siddhartha lamichhane <siddharthalamichhane56@gmail.com>
2025-07-17 22:00:32 +0545

    creating the Award section

--- frontend/src/components/Home/Award.jsx
+++ frontend/src/components/Home/Award.jsx
@@ -1,17 +1,39 @@
 import ecosystem from "../../../src/assets/images/ecosystem.png";
+import presslogos from "../../../src/assets/images/presslogos.png";
 
 export const Award = () => {
   return (
     <>
       <div className="container mb-5">
-        <div className="row">
-          <div className="col-4">
-            <h1>Trust with confidence</h1>
+        <div className="row mb-5">
+          <div className="col-5 ">
+            <h2>Trust with confidence</h2>
+            <div>
+              <h3>Customer-first always</h3>
+              <h4>
+                That's why 1.6+ crore customers trust Zerodha with ~ ₹6 lakh
+                crores of equity investments and contribute to 15% of daily
+                retail exchange volumes in India.
+              </h4>
+              <h3>No spam or gimmicks</h3>
+              <h4>
+                No gimmicks, spam, "gamification", or annoying push
+                notifications. High quality apps that you use at your pace, the
+                way you like. Our philosophies.
+              </h4>
+            </div>
           </div>
           <div className="col-7">
-            <img className="w-76 " src={ecosystem} alt="Ecosystem Image"></img>
+            <img
+              className="img-fluid w-75"
+              src={ecosystem}
+              alt="Ecosystem Image"
+            ></img>
           </div>
         </div>
+      </div>
+      <div className="align-items: center">
+        <img src={presslogos}></img>
       </div>
     </>
   );
