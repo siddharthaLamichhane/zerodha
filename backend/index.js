2957d3a636bb657d1ca8a35480af18804d6610b6
siddhartha lamichhane <siddharthalamichhane56@gmail.com>
2025-12-18 10:20:07 +0545

    setting up basic backend

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from app",
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  mongoose.connect(process.env.MONGO_URL);
+
+  console.log(process.env.MONGO_URL);
+  console.log(`app listening ${process.env.PORT} `);
+});
